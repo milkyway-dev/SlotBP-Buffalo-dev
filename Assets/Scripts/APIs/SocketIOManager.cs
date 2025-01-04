8a03d5ef18dc8636a968053e50eb7d3c83477c29
Mehul Parakh <mehul@underpinservices.com>
2025-01-04 16:40:45 +0530

    Bugs fixes

--- Assets/Scripts/APIs/SocketIOManager.cs
+++ Assets/Scripts/APIs/SocketIOManager.cs
@@ -38,7 +38,7 @@ public class SocketIOManager : MonoBehaviour
 
     protected string SocketURI = null;
     //protected string TestSocketURI = "https://game-crm-rtp-backend.onrender.com/";
-    protected string TestSocketURI = "http://localhost:5000";
+    protected string TestSocketURI = "http://localhost:5001";
 
     [SerializeField]
     private string testToken;
